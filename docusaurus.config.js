4cbc019ff9b94589754b12462540f014900ee617
Nicolas <nicolas.baud12@gmail.com>
2024-03-23 12:48:37 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,7 +22,6 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'SwizCloudFR', // Usually your GitHub org/user name.
   projectName: 'docs', // Usually your repo name.
-  deploymentBranch: 'main',
   trailingSlash: false,
 
   onBrokenLinks: 'throw',
